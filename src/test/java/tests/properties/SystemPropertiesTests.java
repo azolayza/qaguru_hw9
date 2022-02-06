adf53b7d9f77589a61880d49df9c65815c4af9bd
Elizaveta Azovtseva <azovcevae@gmail.com>
2022-02-06 16:06:41 +0700

    add new branch Properties and edit biuld gradle

---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,51 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SystemPropertiesTests {
+
+    //        System.getProperty("key");
+    //        System.setProperty("key", "value");
+
+    @Test
+    void SomeTest1(){
+        String someValue = System.getProperty("someKey");
+        System.out.println(someValue); //null
+
+        System.setProperty("someKey", "redValue");
+        someValue = System.getProperty("someKey");
+        System.out.println(someValue);
+    }
+
+    @Test
+    void SomeTest2(){
+        System.setProperty("someKey", "redValue");
+        String someValue = System.getProperty("someKey");
+        System.out.println(someValue);
+    }
+
+    @Test
+    void SomeTest3(){
+        String someValue = System.getProperty("someKey", "blue value");
+        System.out.println(someValue);
+    }
+
+    @Test
+    void SomeTest4(){
+        Boolean someValue = Boolean.parseBoolean(
+                System.getProperty("someKey", "true"));
+        assertTrue(someValue);
+        System.out.println(someValue);
+    }
+
+    @Test
+    @Tag("properties")
+    void SomeTest5(){
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+}
