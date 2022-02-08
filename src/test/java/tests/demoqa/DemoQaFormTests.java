f484f479b3ecc3dfcba73e88706f00e150832023
Elizaveta Azovtseva <azovcevae@gmail.com>
2022-02-09 01:39:50 +0700

    edit command_line file and test fo DemoQAForm

--- src/test/java/tests/demoqa/DemoQaFormTests.java
+++ src/test/java/tests/demoqa/DemoQaFormTests.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
+@Tag("simple")
 public class DemoQaFormTests extends TestBase {
 
     @Owner("AzovcevaE")
